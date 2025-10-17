0e090ab956a7a49f3c3e56e1a32718591148641c
Hp <kenzamouktabil@gmail.com>
2025-10-17 14:58:16 +0200

    first interface

---
+++ Calculatrice/src/model/CalculatorModelInterface.java
@@ -0,0 +1,17 @@
+package model;
+
+public interface CalculatorModelInterface {
+	
+	void add();
+	void substract();
+	void multipy();
+	void divide();
+	void opposite();
+	
+	void push();
+	void pop();
+	void drop();
+	void swap();
+	void clear();
+
+}
